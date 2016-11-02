1b9d4c1c627191165db42d6c102ccdb91bd59ffa
konstantin8105 <konstantin8105@gmail.com>
2016-11-02 22:29:41 +0300

    refactoring

--- src/test/java/home/solver/StrengthSolverTest.java
+++ src/test/java/home/solver/StrengthSolverTest.java
@@ -1,5 +1,16 @@
 package home.solver;
 
+import home.finiteElements.FemBeam2d;
+import home.finiteElements.interfaces.FemElement;
+import home.other.Direction;
+import home.other.FemPoint;
+import home.other.Force;
+import home.other.Support;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class StrengthSolverTest {
 //
 //    @Test
@@ -95,48 +106,49 @@ public class StrengthSolverTest {
 //        assertEquals(femPoints[amount - 1].getGlobalDisplacement()[1], 0.2083333, 1e-4);
 //    }
 //
-//    @Test
-//    public void testBookPage535() {
-//        double L = 1;
-//        FemPoint[] femPoints = new FemPoint[2];
-//        femPoints[0] = new FemPoint(0, 0.0, 0);
-//        femPoints[1] = new FemPoint(1, L, 0);
-//
-//        double E = 200000e6;
-//        double I = 78e-8;
-//
-//        double elacity = E;
-//        double inertia = I;
-//        double area = 1e-13;
-//        double Q = 1230;
-//
-//        iModal[] femElements = new iModal[1];
-//        femElements[0] = new FemBeam2d(elacity, area, inertia, new FemPoint[]{femPoints[0], femPoints[1]});
-//
-//        Support[] supports = new Support[]{
-//                new Support(femPoints[0], Direction.DIRECTION_X),
-//                new Support(femPoints[0], Direction.DIRECTION_Y),
-//                new Support(femPoints[0], Direction.ROTATE),
-//        };
-//
-//        Force[] forces = new Force[]{
-//                new Force(femPoints[1], Direction.DIRECTION_Y, Q)
-//        };
-//        //=========================//
-//        boolean exception = false;
-//        try {
-//            StrengthSolver.calculate(femPoints, femElements, forces, supports);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            exception = true;
-//        }
-//
-//        assertFalse(exception);
-//
-//        double deflection = Q * Math.pow(L, 3.) / (3 * E * I);
-//        System.out.println("Deflection = " + deflection);
-//
-//        assertEquals(femPoints[1].getGlobalDisplacement()[0], 0.0000, 1e-4);
-//        assertEquals(femPoints[1].getGlobalDisplacement()[1], deflection, 1e-4);
-//    }
+    @Test
+    public void testBookPage535() {
+        double L = 1;
+        FemPoint[] femPoints = new FemPoint[2];
+        femPoints[0] = new FemPoint(0, 0.0, 0);
+        femPoints[1] = new FemPoint(1, L, 0);
+
+        double E = 200000e6;
+        double I = 78e-8;
+
+        double elacity = E;
+        double inertia = I;
+        double area = 1e-13;
+        double Q = 1230;
+
+        FemElement[] femElements = new FemElement[1];
+        femElements[0] = new FemBeam2d(elacity, area, inertia, new FemPoint[]{femPoints[0], femPoints[1]});
+
+        Support[] supports = new Support[]{
+                new Support(femPoints[0], Direction.DIRECTION_X),
+                new Support(femPoints[0], Direction.DIRECTION_Y),
+                new Support(femPoints[0], Direction.ROTATE),
+        };
+
+        Force[] forces = new Force[]{
+                new Force(femPoints[1], Direction.DIRECTION_Y, Q)
+        };
+        //=========================//
+        boolean exception = false;
+        StrengthSolver solver = null;
+        try {
+            solver = new StrengthSolver(femPoints, femElements, forces, supports);
+        } catch (Exception e) {
+            e.printStackTrace();
+            exception = true;
+        }
+
+        assertFalse(exception);
+
+        double deflection = Q * Math.pow(L, 3.) / (3 * E * I);
+        System.out.println("Deflection = " + deflection);
+
+        assertEquals(solver.getGlobalDeformationPoint(femPoints[1]).getX(), 0.0000, 1e-4);
+        assertEquals(solver.getGlobalDeformationPoint(femPoints[1]).getY(), deflection, 1e-4);
+    }
 }
