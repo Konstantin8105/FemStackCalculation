36fb57e1a4a858e94e5c140cbc0b4d2e0795b1a1
Konstantin8105 <pollygon2011>
2016-11-03 17:01:36 +0300

    refactoring

--- src/main/java/home/solver/FemModel.java
+++ src/main/java/home/solver/FemModel.java
@@ -1,5 +1,7 @@
 package home.solver;
 
+import Jama.Matrix;
+import home.finiteElements.FemTruss2d;
 import home.finiteElements.interfaces.FemElement;
 import home.other.FemPoint;
 import home.other.Support;
@@ -10,6 +12,30 @@ public class FemModel {
             FemPoint[] femPoints,
             FemElement[] femElements,
             Support[] supports) throws Exception {
+
+        int aColumn = 3 * femPoints.length;
+        int aRow = 3 * femElements.length;
+
+
+        for (FemElement element : femElements) {
+            Matrix globalStiffener = element.getStiffenerMatrixTr();
+            globalStiffener.print(15, 1);
+
+
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        FemPoint[] femPoints = new FemPoint[2];
+        femPoints[0] = new FemPoint(0, 0, 0);
+        femPoints[1] = new FemPoint(0, 1, 0);
+
+        FemTruss2d fem = new FemTruss2d(1, 1, new FemPoint[]{femPoints[0], femPoints[1]});
+
+        Matrix globalStiffener = fem.getStiffenerMatrixTr();
+        globalStiffener.print(15, 1);
 
     }
 }
